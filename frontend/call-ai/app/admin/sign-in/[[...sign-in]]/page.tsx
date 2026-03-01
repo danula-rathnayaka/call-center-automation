d32dfe89e71689a8c7691ef6bc22434462279ea6
Arosha Withanage <avwithanage2006@gmail.com>
2026-03-01 11:08:32 +0530

    feat: update sign in and up ui

--- frontend/call-ai/app/admin/sign-in/[[...sign-in]]/page.tsx
+++ frontend/call-ai/app/admin/sign-in/[[...sign-in]]/page.tsx
@@ -3,5 +3,13 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <>
+      <div className="flex min-h-screen bg-neutral-50 text-neutral-800">
+        <main className="flex-1 flex justify-center items-center p-10 border-x border-neutral-200">
+          <SignIn signUpUrl="/admin/sign-up" />
+        </main>
+      </div>
+    </>
+  );
 }
--- frontend/call-ai/app/admin/sign-up/[[...sign-up]]/page.tsx
+++ frontend/call-ai/app/admin/sign-up/[[...sign-up]]/page.tsx
@@ -3,5 +3,13 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <>
+      <div className="flex min-h-screen bg-neutral-50 text-neutral-800">
+        <main className="flex-1 flex justify-center items-center p-10 border-x border-neutral-200">
+          <SignUp signInUrl="/admin/sign-in" />
+        </main>
+      </div>
+    </>
+  );
 }
