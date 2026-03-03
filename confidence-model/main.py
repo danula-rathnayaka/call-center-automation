10f88aec433d31c726b555d00a50de2403f589f9
Arosha Withanage <avwithanage2006@gmail.com>
2026-03-03 21:03:15 +0530

    feat: add model pipeline

--- confidence-model/main.py
+++ confidence-model/main.py
@@ -1,6 +1,56 @@
-def main():
-    print("Hello from confidence-model!")
+from features.confidence_feature_extractor import ConfidenceFeatureExtractor
+from sentence_transformers import SentenceTransformer
+from sklearn.preprocessing import StandardScaler
+
+import numpy as np
+from xgboost import XGBClassifier
+import re
+import pickle
+
+class ConfidentModel:
+    def __init__(self):
+        self.confidentExtractor = ConfidenceFeatureExtractor()
+        self.embedder = SentenceTransformer("all-MiniLM-L6-v2")
+        self.model = XGBClassifier()
+        self.model.load_model('model/best_xgb.json')
+        self.threshold = 0.5
+
+        with open("model/scaler.pkl", "rb") as f:
+            self.scaler = pickle.load(f)
+
+    
+
+    def clean_text(self, text):
+        text = str(text).lower().strip()
+        text = re.sub(r"\s+", " ", text)
+        return text
 
 
-if __name__ == "__main__":
-    main()
+    def predict_confident_level(self, text: str):
+        """Predict confidence level for a given text."""
+        text = self.clean_text(text)
+
+        # Generate embedding
+        emb = self.embedder.encode([text])
+
+        ling = self.confidentExtractor.extract_features(text)
+        ling_vec = np.array(list(ling.values())).reshape(1, -1)
+        ling_vec_scaled = self.scaler.transform(ling_vec)
+
+        # Combine features
+        X_input = np.hstack([emb, ling_vec_scaled])
+
+        # Predict
+        prob = self.model.predict_proba(X_input)[0][1]
+        label = "high" if prob >= self.threshold else "low"
+
+        return {
+            "confidence_label": label,
+            "confidence_score": float(prob),
+            "linguistic_features": ling
+        }
+        
+
+
+cm = ConfidentModel()
+print(cm.predict_confident_level("I don't know, perhaps it might work"))
